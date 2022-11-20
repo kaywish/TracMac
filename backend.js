3a5871e2ad7d1761ad204d278c96dcdc3567e172
kaywish <kaywishawaan@gmail.com>
2022-11-20 13:04:47 -0500

    make it better

--- backend.js
+++ backend.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express()
-
+//import cors
+const cors = require('cors')
 const routes = require('./routes')
 
 
@@ -14,8 +15,9 @@ require('dotenv').config()
 const bcrypt = require('bcrypt')
 
 /* == Middleware == */
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors("*"))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 // Routes 
 app.use("/food", routes.macros)
